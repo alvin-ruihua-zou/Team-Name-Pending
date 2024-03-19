456d9a90e5921d514caf893e587c6a30f4530085
YN-Zhao-07 <yunuozhao@outlook.com>
2024-03-19 19:44:25 -0400

    Can do direction and revolution inputs on Nano

--- main.py
+++ main.py
@@ -3,40 +3,48 @@
 import serial
 from pynput import keyboard
 
-arduino = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
+arduino = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 
-
+input_str = ""
 def on_press(key):
+    global input_str
     try:
         key = key.char
         print("\nYou Entered {0}".format(key))
         if key == "w":
-            arduino.write(bytes("w", "utf-8"))
+            input_str += key
         if key == "t":
-            arduino.write(bytes("t", "utf-8"))
+            input_str += key
         if key == "a":
-            arduino.write(bytes("a", "utf-8"))
+            input_str += key
         if key == "d":
-            arduino.write(bytes("d", "utf-8"))
+            input_str += key
         if key == "z":
-            arduino.write(bytes("z", "utf-8"))
+            input_str += key
         if key == "p":
-            arduino.write(bytes("p", "utf-8"))
+            input_str += key
         if key == "v":
-            arduino.write(bytes("v", "utf-8"))
+            input_str += key
         if key == "b":
-            arduino.write(bytes("b", "utf-8"))
+            input_str += key
         if key == "n":
-            arduino.write(bytes("n", "utf-8"))
+            input_str += key
         if key == "m":
-            arduino.write(bytes("m", "utf-8"))
+            input_str += key
         if key == "i":
-            arduino.write(bytes("i", "utf-8"))
+            input_str += key
+       
+        if key in [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            input_str += key
 
     except AttributeError:
         print("\nYou Entered {0}".format(key))
-        if key == keyboard.Key.up:
-            arduino.write(bytes("up\n", "utf-8"))
+        if key == keyboard.Key.space:
+            input_str += " "
+
+        if key == keyboard.Key.enter:
+            arduino.write(bytes(input_str, "utf-8"))
+            input_str = ""
         if key == keyboard.Key.esc:
             return False
 
