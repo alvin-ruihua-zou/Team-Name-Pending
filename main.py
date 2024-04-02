3d629c2d788e45852b906b49d0a360e9dcc91851
Alvin Zou <80643461+alvin-ruihua-zou@users.noreply.github.com>
2024-04-02 17:57:09 -0400

    Modified Jetson communication

--- main.py
+++ main.py
@@ -3,9 +3,11 @@
 import serial
 from pynput import keyboard
 
-arduino = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
+arduino = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
 
 input_str = ""
+
+
 def on_press(key):
     global input_str
     try:
@@ -33,29 +35,21 @@ def on_press(key):
             input_str += key
         if key == "i":
             input_str += key
-       
-        if key in [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+        if key in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]:
             input_str += key
 
     except AttributeError:
         print("\nYou Entered {0}".format(key))
-        if key == keyboard.Key.space:
-            input_str += " "
-
         if key == keyboard.Key.enter:
-            arduino.write(bytes(input_str, "utf-8"))
+            arduino.write(bytes(input_str + "\r\n", "utf-8"))
             input_str = ""
         if key == keyboard.Key.esc:
             return False
 
 
-# print("Start")
-# with keyboard.Listener(on_press=on_press) as listener:
-#     listener.join()
-
-# print("done")
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
+# listener = keyboard.Listener(on_press=on_press)
+# listener.start()
 
 while True:
-    continue
+    input = input("Enter command ")
+    arduino.write(bytes(input + "\r\n", "utf-8"))
--- servo_test_v2_mega/servo_test_v2_mega.ino
+++ servo_test_v2_mega/servo_test_v2_mega.ino
@@ -1,6 +1,6 @@
 
 #include <Encoder.h>
-//#include "Servo.h"
+// #include "Servo.h"
 #include "MultiServo.h"
 #include "Steppers.h"
 // Change these two numbers to the pins connected to your encoder.
@@ -33,19 +33,20 @@ const int s2step = 31;
 const int voltPin = A15;
 
 // end pindef__________________________________________________________________
-long currPosition  = 0;
+long currPosition = 0;
 long currPosition2 = 0;
-//Encoder myEnc(2, 8);
+// Encoder myEnc(2, 8);
 Servo s1(z1en0, z1en1, z1pwm, 19, 42);
 Servo s2(z2en0, z2en1, z2pwm, 18, 43);
 Servo d1(d1en0, d1en1, d1pwm, 3, 40);
 Servo d2(d2en0, d2en1, d2pwm, 2, 41);
 MultiServo zAxis(s1, s2);
-MultiServo driveTrain(d1,d2);
+MultiServo driveTrain(d1, d2);
 Steppers xAxis(s1dir, s1step, s2dir, s2step);
 //   avoid using pins with LEDs attached
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   Serial.println("Basic Encoder Test:");
 
@@ -65,7 +66,6 @@ void setup() {
   pinMode(d2en1, OUTPUT);
   pinMode(d2pwm, OUTPUT);
 
-
   pinMode(s1dir, OUTPUT);
   pinMode(s1step, OUTPUT);
   pinMode(s2dir, OUTPUT);
@@ -73,16 +73,17 @@ void setup() {
 
   pinMode(A0, INPUT);
 
-
-  //s1.set_current_limiting(400, true);
+  // s1.set_current_limiting(400, true);
 }
 
-double get_voltage(){
+double get_voltage()
+{
   double voltage = (double)analogRead(voltPin) / 1023 * 55.7;
   return voltage;
 }
 
-void drive_till_edge(int speed, int max_range){
+void drive_till_edge(int speed, int max_range)
+{
   long initPos = d1.myEnc.read();
   long currPos = d1.myEnc.read();
   int thresh = 150;
@@ -90,56 +91,66 @@ void drive_till_edge(int speed, int max_range){
   // Serial.println(abs(currPos - initPos));
   // Serial.println(analogRead(A9) < thresh);
   // Serial.println(abs(currPos - initPos) < max_range);
-  while((analogRead(A9) > thresh) && (abs(currPos - initPos) < max_range)){
+  while ((analogRead(A9) > thresh) && (abs(currPos - initPos) < max_range))
+  {
     Serial.println(analogRead(A9));
     currPos = d1.myEnc.read();
-    if(speed < 0){
+    if (speed < 0)
+    {
       d1.bw(-speed);
       d2.fw(-speed);
-    }else{
+    }
+    else
+    {
       d1.fw(speed);
       d2.bw(speed);
     }
-    
   }
   d1.mbreak();
   d2.mbreak();
 }
-void cmd_servo_multi(){
+void cmd_servo_multi()
+{
   Serial.println("Enter CMD:");
-  while (Serial.available() == 0){
+  while (Serial.available() == 0)
+  {
     delay(10);
   }
   String cmd = Serial.readString();
-  
+
   int index = 0;
-  for(int i = 0; i < 5 && i < cmd.length(); i++){
-    if(isDigit(cmd[i]) or cmd[i] == '-'){
+  for (int i = 0; i < 5 && i < cmd.length(); i++)
+  {
+    if (isDigit(cmd[i]) or cmd[i] == '-')
+    {
       index = i;
       break;
     }
   }
-  if(index == 0){
+  if (index == 0)
+  {
     index = cmd.length();
   }
-  String dir = cmd.substring(0,index);
-  double rev =cmd.substring(index).toFloat();
+  String dir = cmd.substring(0, index);
+  double rev = cmd.substring(index).toFloat();
   Serial.print("cmd is:");
   Serial.println(dir);
   Serial.print("rev is:");
   Serial.println(rev);
-  
+  return;
   // if(dir == 'w'){
   //   s1.servo_to(tick1, 160, 0.2, 0.1);
   // }
-  if(dir == 'w'){
+  if (dir == 'w')
+  {
     long currPosition1 = s1.myEnc.read();
     long currPosition2 = s2.myEnc.read();
     long tick1 = -rev * 1836 + currPosition1;
     long tick2 = rev * 1836 + currPosition2;
     zAxis.servo_to(tick1, tick2, 40, 0.3, 0.1);
   }
-  if(dir == "t"){
+  if (dir == "t")
+  {
     Serial.println("poi");
     long currPosition1 = d1.myEnc.read();
     long currPosition2 = d2.myEnc.read();
@@ -147,14 +158,16 @@ void cmd_servo_multi(){
     long tick2 = rev * 792 + currPosition2;
     driveTrain.servo_to(tick1, tick2, 150, 1.2, 0.1, true);
   }
-  if(dir == "fw"){
+  if (dir == "fw")
+  {
     long currPosition1 = d1.myEnc.read();
     long currPosition2 = d2.myEnc.read();
     long tick1 = rev * 792 + currPosition1;
     long tick2 = -rev * 792 + currPosition2;
     driveTrain.servo_to(tick1, tick2, 120, 0.5, 0.2, true);
   }
-  if(dir == "l"){
+  if (dir == "l")
+  {
     long currPosition1 = d1.myEnc.read();
     long currPosition2 = d2.myEnc.read();
     long tick1 = 0.4 * 1836 + currPosition1;
@@ -165,83 +178,93 @@ void cmd_servo_multi(){
     tick2 = -1.0 * 1836 + currPosition2;
     driveTrain.servo_to_no_correction(tick1, tick2, 120, 0.4, 0.1, true);
   }
-  if(dir == "a"){
-    xAxis.revStepperSRamp(abs(rev), 1, 7 );
+  if (dir == "a")
+  {
+    xAxis.revStepperSRamp(abs(rev), 1, 7);
   }
-  if(dir == "d"){
-    xAxis.revStepperSRamp(abs(rev), -1, 7 );
+  if (dir == "d")
+  {
+    xAxis.revStepperSRamp(abs(rev), -1, 7);
   }
-  if(dir == "z"){
+  if (dir == "z")
+  {
     s1.myEnc.write(0);
     s2.myEnc.write(0);
     driveTrain.clear_odo();
   }
-   if(dir == "p"){
+  if (dir == "p")
+  {
     Serial.print("s1: ");
     Serial.println(s1.myEnc.read());
     Serial.print("s1: ");
     Serial.println(s2.myEnc.read());
   }
-  if(dir == "v"){
-    zAxis.servo_to(4.3 * 1836, -4.3* 1836, 80, 0.4, 0.1);
+  if (dir == "v")
+  {
+    zAxis.servo_to(4.3 * 1836, -4.3 * 1836, 80, 0.4, 0.1);
   }
-  if(dir == "b"){
-    xAxis.revStepperSRamp(6.4, -1, 6 );
+  if (dir == "b")
+  {
+    xAxis.revStepperSRamp(6.4, -1, 6);
   }
-  if(dir == "n"){
+  if (dir == "n")
+  {
     zAxis.servo_to(-0.4 * 1836, 0.4 * 1836, 80, 0.4, 0.1);
   }
-  if(dir == "m"){
-    xAxis.revStepperSRamp(6.4, 1, 6 );
+  if (dir == "m")
+  {
+    xAxis.revStepperSRamp(6.4, 1, 6);
   }
 
-  if(dir == "i"){
-    for(int i = 0; i < 3; i++){
-      zAxis.servo_to(4.3 * 1836, -4.3* 1836, 100, 0.4, 0.1);
-    delay(200);
-    xAxis.revStepperSRamp(6.4, -1, 6 );
-    delay(200);
+  if (dir == "i")
+  {
+    for (int i = 0; i < 3; i++)
+    {
+      zAxis.servo_to(4.3 * 1836, -4.3 * 1836, 100, 0.4, 0.1);
+      delay(200);
+      xAxis.revStepperSRamp(6.4, -1, 6);
+      delay(200);
 
-    
-    
-    zAxis.servo_to(-0.4 * 1836, 0.4 * 1836, 100, 0.4, 0.1);
-    delay(200);
-    xAxis.revStepperSRamp(6.4, 1, 6 );
-    delay(200);
+      zAxis.servo_to(-0.4 * 1836, 0.4 * 1836, 100, 0.4, 0.1);
+      delay(200);
+      xAxis.revStepperSRamp(6.4, 1, 6);
+      delay(200);
 
-    long currPosition1 = d1.myEnc.read();
-    long currPosition2 = d2.myEnc.read();
-    long tick1 = 0.48 * 792 + currPosition1;
-    long tick2 = -0.48 * 792 + currPosition2;
-    driveTrain.servo_to_no_correction(tick1, tick2, 200, 1.9, 0.1, false);
-    delay(200);
-    tick1 = 1.0 * 792 + currPosition1;
-    tick2 = -1.0 * 792 + currPosition2;
-    driveTrain.servo_to_no_correction(tick1, tick2, 100, 0.5, 0.1, true);
-    delay(200);
+      long currPosition1 = d1.myEnc.read();
+      long currPosition2 = d2.myEnc.read();
+      long tick1 = 0.48 * 792 + currPosition1;
+      long tick2 = -0.48 * 792 + currPosition2;
+      driveTrain.servo_to_no_correction(tick1, tick2, 200, 1.9, 0.1, false);
+      delay(200);
+      tick1 = 1.0 * 792 + currPosition1;
+      tick2 = -1.0 * 792 + currPosition2;
+      driveTrain.servo_to_no_correction(tick1, tick2, 100, 0.5, 0.1, true);
+      delay(200);
     }
-    
   }
-  if(dir == "di"){
+  if (dir == "di")
+  {
     drive_till_edge(-90, 2000);
-    delay(500);                                                                                                                                                                         
+    delay(500);
     delay(200);
     long currPosition1 = d1.myEnc.read();
     long currPosition2 = d2.myEnc.read();
     long tick1 = -0.18 * 792 + currPosition1;
     long tick2 = 0.18 * 792 + currPosition2;
     driveTrain.servo_to_no_correction(tick1, tick2, 200, 4.5, 0.1, false);
-    
   }
-  if(dir == "?"){
+  if (dir == "?")
+  {
     Serial.print("Votage is: ");
     Serial.println(get_voltage());
   }
-  if(dir == "dte"){
+  if (dir == "dte")
+  {
     Serial.println("poi");
     drive_till_edge(rev, 2000);
   }
-  if(dir == "odo"){
+  if (dir == "odo")
+  {
     Serial.print("x, y, theta");
     Serial.println(driveTrain.dx);
     Serial.println(driveTrain.dy);
@@ -258,7 +281,7 @@ void cmd_servo_multi(){
 //   Serial.println(cmd);
 //   char dir = cmd[0];
 //   double rev =cmd.substring(1).toFloat();
-  
+
 //   currPosition = s1.myEnc.read();
 //   int tick = rev * 792 + currPosition;
 //   if(dir == 'w'){
@@ -271,9 +294,7 @@ void cmd_servo_multi(){
 //     s2.servo_to(tick2, 160, 0.2, 0.1);
 //   }
 // }
-void loop() {
+void loop()
+{
   cmd_servo_multi();
-
-
-
 }
