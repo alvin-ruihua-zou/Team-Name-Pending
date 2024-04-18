690f81f91a34000ac006e7b95efe1ec4ab113037
kevinPOI <kevin_tang233@outlook.com>
2024-04-18 18:28:09 -0400

    added max len

--- main.py
+++ main.py
@@ -228,9 +228,7 @@ def navigation2climbing(
     counter = 0;
     max_len = 0;
     while True:
-        print("cmd_sequence len", max_len)
-        print("counter is: ", counter)
-        print("curr pose:", curr_pos, "goal pos:", temp_goal)
+        
         if counter == 0:
             max_len = len(cmd_sequence) + 1
             counter = 1
@@ -239,6 +237,10 @@ def navigation2climbing(
         if counter >= max_len:
             if curr_pos[2] == temp_goal[2]:
                 complete
+
+        print("cmd_sequence len", max_len)
+        print("counter is: ", counter)
+        print("curr pose:", curr_pos, "goal pos:", temp_goal)
         cmd_sequence, curr_pos, complete, cmd = plan(start=curr_pos, goal=temp_goal)
        
         
