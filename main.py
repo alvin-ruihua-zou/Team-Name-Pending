8483fe134b4de1c548482119ccd8d90df8b2ba4f
Alvin Zou <80643461+alvin-ruihua-zou@users.noreply.github.com>
2024-04-19 21:17:19 -0400

    Removed support for bw

--- main.py
+++ main.py
@@ -109,21 +109,16 @@ def plan(
     for i in range(len(prim_id_commands)):
         id = prim_id_commands[i]
         prim = prims_dict[id]
-        # fw/bw command if angle doesn't change
+        # fw command if angle doesn't change
         if prim.start_angle == prim.endpose[2]:
             if prim.endpose[0] == 0:
                 dist = abs(prim.endpose[1]) * arduino_fw_conversion
             else:
                 dist = abs(prim.endpose[0]) * arduino_fw_conversion
-            # bw
-            if prim.endpose[0] < 0 or prim.endpose[1] < 0:
-                cmd = "bw" + str(dist)
+            cmd = "fw" + str(dist)
+            if prev_cmd != "fw":
+                prev_cmd = "fw"
 
-            # fw
-            else:
-                cmd = "fw" + str(dist)
-            if i == 0:
-                prev_cmd = cmd[:2]
         # t command since robot is turning
         else:
             angle = (
@@ -133,32 +128,16 @@ def plan(
                 * arduino_t_conversion
             )
             cmd = f"t{angle:.3f}"
-        if cmd[0] == "t":
-            if i == 0:
-                cmf_sequence = cmd + ":"
+        if cmd[:2] == "fw" and prev_cmd == "fw":
+            # Append cmd if it's the last one
+            if i == len(prim_id_commands) - 1:
+                cmd_sequence += cmd + ":"
             else:
-                cmd_sequence += prev_cmd + str(fw_sum) + ":" + cmd + ":"
+                fw_sum += dist
+        else:
+            cmd_sequence += "fw" + str(fw_sum) + ":" + cmd + ":"
             fw_sum = 0
             prev_cmd = "t"
-        elif cmd[:2] == "fw" and prev_cmd == "fw":
-            # Append cmd if it's the last one
-            if i == len(prim_id_commands) - 1:
-                cmd_sequence += cmd + ":"
-            else:
-                fw_sum += dist
-        elif cmd[:2] == "bw" and prev_cmd == "bw":
-            # Append cmd if it's the last one
-            if i == len(prim_id_commands) - 1:
-                cmd_sequence += cmd + ":"
-            else:
-                fw_sum += dist
-        # TODO: Switching between fw and bw
-
-        else:
-            cmd_sequence += prev_cmd + str(fw_sum) + ":"
-            prev_cmd = cmd
-            fw_sum = 0
-
     cmd_sequence = cmd_sequence.split(":")[:-1]
     cmd = cmd_sequence[0]
     if "fw" in cmd:
